f2e9183b983f40b949aa24ba655b26d19f158654
Pragatha02 <mpragatha063@gmail.com>
2025-10-30 11:05:15 +0530

    File added

---
+++ JavaLearning/src/com/Day2/Day2hw2.java
@@ -0,0 +1,13 @@
+package com.Day2;
+
+public class Day2hw2 {
+    public static void main(String[] args){
+        if(args.length==2){
+            System.out.println( args[0] + " Technologies " + args[1]);
+        }
+        else{
+            System.out.println("Pass two arguments exactly");
+        }
+    }
+
+}
