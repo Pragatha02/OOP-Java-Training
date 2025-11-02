7fb399c005ef659ff75caf849c0de4793b0696a3
Pragatha02 <mpragatha063@gmail.com>
2025-11-02 21:36:56 +0530

    file added

--- JavaLearning/src/com/Day2/Day2hw3.java
+++ JavaLearning/src/com/Day2/Day2hw3.java
@@ -2,10 +2,7 @@ package com.Day2;
 
 public class Day2hw3 {
     public static void main(String[] args) {
-
-            // args[0] contains the first command line argument
             String name = args[0];
-
             System.out.println("Welcome " + name);
         }
 }
