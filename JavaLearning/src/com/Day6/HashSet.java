a15f33a49c8333f339866751e2916e03a099d032
Pragatha02 <mpragatha063@gmail.com>
2025-11-04 11:14:22 +0530

    file added

---
+++ JavaLearning/src/com/Day6/HashSet.java
@@ -0,0 +1,29 @@
+package com.Day6;
+
+/*
+Hashset is a class implementing set interface
+Heterogenous data are allowed
+Duplicates are not allowed
+single null value
+won't maintain insertion order
+
+ */
+/*
+ * 1. Declaration
+ * 2. Adding Elements
+ * 3. Printing HashSet
+ * 4. Printing size
+ * 5. Remove specific value
+ * 6. Inserting Element
+ * 7. Accessing specific element
+ * 8. Alternative method to access the specific element
+ * 9. Read all the elements
+ * 10. clear all elements
+ * 11. check whether the hashset is empty
+ */
+
+import java.util.*;
+
+public class HashSet {
+
+}
